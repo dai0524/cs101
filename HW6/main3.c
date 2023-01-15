a32c220d3887ca606d26d70ce976d9df629966ad
dai0524 <120006145+dai0524@users.noreply.github.com>
2023-01-15 23:27:15 +0800

    Create main3.c

---
+++ HW6/main3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int year = 2022;
+    
+    if ((year % 4)== 0 && (year % 100)!= 0 || (year % 400) == 0) {
+ printf("\t%d是閏年\n" , year);
+ } else {
+  printf("\t%d不是閏年\n" , year);
+ } 
+    return 0;
+}
